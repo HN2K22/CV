585e62975c8ccb2ff7ba2e6488b2116b96f6e357
HN2K22 <117502564+HN2K22@users.noreply.github.com>
2025-02-14 18:40:27 +0530

    Update: Removed JS content will be updated later with planning

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("download-pdf").addEventListener("click", () => {
-    const element = document.getElementById("content");
-    const options = {
-      filename: "GFG.pdf",
-      margin: 0,
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-    };
 
-    html2pdf().set(options).from(element).save();
-  });
-});
