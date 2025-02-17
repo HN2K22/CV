7e8947a606c72e5f1e29830fd1cfc1c4c089d84a
HN2K22 <117502564+HN2K22@users.noreply.github.com>
2025-02-17 20:18:54 +0530

    Update: Moved href from HTML to JavaScript event listeners - Moved from html so even if someone tinkers in html js will back it Update

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const linkedin = document.querySelector(".linkedin");
+const gmail = document.querySelector(".gmail");
+const github = document.querySelector(".github");
+const cv = document.querySelector(".cv");
 
+const linkArray = [
+  {
+    button: linkedin,
+    link: "https://www.linkedin.com/in/hitesh-nagvekar-a09707b5/",
+  },
+  {
+    button: gmail,
+    link: "mailto:hiteshlevis1@gmail.com",
+  },
+  {
+    button: github,
+    link: "https://github.com/HN2K22",
+  },
+  {
+    button: cv,
+    link: "https://hn2k22.github.io/CV/",
+  },
+];
+
+function clickEvents(button, link) {
+  if (button) {
+    button.addEventListener("click", () => {
+      document.location.href = link;
+    });
+  }
+}
+
+linkArray.forEach(({ button, link }) => {
+  clickEvents(button, link);
+});
